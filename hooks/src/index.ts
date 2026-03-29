71bed35315ea7e5f561d003ae103ec5952beaea9
PDGamerSG <pallab4586das@gmail.com>
2026-03-29 15:03:41 +0530

    Created metadata,zapier run and zapier hook

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,16 +1,38 @@
 import express from "express"
+import { prisma } from "../lib/prisma.ts";
 
 const app = express();
 
 //the url looks like
 //https://hooks.zapier.com/hooks/catch/1723049/231213/
 
-
-app.post("/hooks/catch/:userId/:zapId",(req,res) =>{
+app.post("/hooks/catch/:userId/:zapId", async (req,res) =>{
     const userId = req.params.userId;
     const zapId = req.params.userId;
+    const body = req.body;
 
     //store in db a new trigger
+    await prisma.$transaction(async tx =>{
+        const run  = await prisma.zapRun.create({
+            data:{
+                zapId:zapId,
+                metadata: body
+            }
+        });
+        await prisma.zapRunOutbox.create({
+            data:{
+                zapRunId : run.id
+            }
+        })
 
+    })
+    await prisma.zapRun.create({
+        data:{
+            zapId: zapId,
+        }
+    })
     //push it on to a queue(kafka/redis)
+    kafkaPublisher.publish({
+
+    })
 })
