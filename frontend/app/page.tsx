b044235f0866021b10fb838275fd1337ab3f8e8b
PDGamerSG <pallab4586das@gmail.com>
2026-04-05 15:34:10 +0530

    Created nav bar

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Appbar } from "@/components/Appbar";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+        <Appbar />
+        {/* <Hero />
+        <HeroVideo /> */}
+    </main>
+  );
+}
